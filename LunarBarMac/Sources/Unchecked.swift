6c8d862eb678c2bd96c8a46977e90472317ee1bc
Gnohz Gniy <0x00eeee@gmail.com>
2024-06-12 15:10:32 +0800

    Fix building issues

--- LunarBarMac/Sources/Unchecked.swift
+++ LunarBarMac/Sources/Unchecked.swift
@@ -5,6 +5,8 @@
 //  Created by cyan on 2024/4/17.
 //
 
+import AppKit
 import EventKit
 
-extension EKEventStore: @unchecked Sendable {}
+extension EKEventStore: @unchecked @retroactive Sendable {}
+extension NSImage: @unchecked @retroactive Sendable {}
