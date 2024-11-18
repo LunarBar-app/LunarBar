531781c8f8fa10ed717430dd7661fc156319ff40
Gnohz Gniy <0x00eeee@gmail.com>
2024-11-18 18:50:14 +0800

    Improve overall readability

--- LunarBarMac/Modules/Sources/AppKitExtensions/NSColor+Extension.swift
+++ LunarBarMac/Modules/Sources/AppKitExtensions/NSColor+Extension.swift
@@ -7,6 +7,12 @@
 import AppKit
 
 public extension NSColor {
+  static var highlightedBackground: NSColor {
+    NSColor(name: nil) {
+      ($0.isDarkMode ? NSColor.white : NSColor.black).withAlphaComponent(0.06)
+    }
+  }
+
   @MainActor
   func resolvedColor(with appearance: NSAppearance = NSApp.effectiveAppearance) -> NSColor {
     var cgColor: CGColor?
