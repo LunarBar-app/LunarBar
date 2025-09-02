4c6050311add1a8c96b3477eb8d5d6ecebcabb71
Ying Zhong <0x00eeee@gmail.com>
2025-09-02 08:29:21 +0800

    Upgrade SwiftLint (#112)

--- LunarBarTools/Package.swift
+++ LunarBarTools/Package.swift
@@ -15,8 +15,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "SwiftLintBinary",
-      url: "https://github.com/realm/SwiftLint/releases/download/0.59.1/SwiftLintBinary.artifactbundle.zip",
-      checksum: "b9f915a58a818afcc66846740d272d5e73f37baf874e7809ff6f246ea98ad8a2"
+      url: "https://github.com/realm/SwiftLint/releases/download/0.61.0/SwiftLintBinary.artifactbundle.zip",
+      checksum: "b765105fa5c5083fbcd35260f037b9f0d70e33992d0a41ba26f5f78a17dc65e7"
     ),
     .plugin(
       name: "SwiftLint",
